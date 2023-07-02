426c76821dd38b54392fc5d8852c5b4854935344
Caroline Faria <caroline.gdfaria@gmail.com>
2023-07-02 19:00:05 -0300

    :sparkles: feat: funcionalidade de editar completa

--- js/patchComment.js
+++ js/patchComment.js
@@ -8,7 +8,7 @@ function patchComment() {
             const paragraph = commentSection.querySelector('.text');
             paragraph.contentEditable = true;
 
-            // check if the "Save" button already exists
+            // Check if the "Save" button already exists
             const saveBtn = commentSection.querySelector('.save-btn');
 
             if (!saveBtn) {
@@ -20,7 +20,37 @@ function patchComment() {
                 newSaveBtn.addEventListener('click', () => {
                     paragraph.contentEditable = false;
                     newSaveBtn.style.display = 'none';
-                    // add save logic here
+                    newSaveBtn.remove();
+
+                    const commentId = paragraph.dataset.currentId;
+                    const updatedComment = {
+                        "author": {
+                            "name": "Grace",
+                            "image": "https://cdn.pixabay.com/photo/2013/07/12/19/28/grace-hopper-154833_960_720.png"
+                        },
+                        "meta": {
+                            "votes": 0
+                        },
+                        "body": `${paragraph.textContent}`
+                    };
+
+                    console.log(JSON.stringify(updatedComment))
+
+                    // send the updated comment data to the server
+                    fetch(`https://comments-l19n.onrender.com/comments/${commentId}`, {
+                        method: 'PATCH',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(updatedComment), // convert the data to JSON string
+                    })
+                        .then(response => response.json())
+                        .then(data => {
+                            console.log('Comment updated successfully!', data);
+                        })
+                        .catch(error => {
+                            console.error('Error updating the comment:', error);
+                        });
                 });
 
                 commentSection.appendChild(newSaveBtn);
@@ -28,4 +58,5 @@ function patchComment() {
         });
     });
 }
+
 
